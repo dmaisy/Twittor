cd1d5f4aff0764fbf0c382f58afa20c5f33dfb5c
dmaisy <dmaisy@gmail.com>
2020-02-03 14:19:04 -0300

    123

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 importScripts ('js/sw-Utils.js');
 
 
-const STATIC_CACHE      ='static-v1';
-const DYNAMIC_CACHE     ='dynamic-v1';
-const INMUTABLE_CACHE   ='inmutable-v1';
+const STATIC_CACHE      ='static-v1'; // Cache del proyecto
+const DYNAMIC_CACHE     ='dynamic-v1'; // Cache dinamico. Archivos de terceros que son modificados.
+const INMUTABLE_CACHE   ='inmutable-v1'; // 
 
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',

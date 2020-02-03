c4e9bb680dd16c39838c5ccfdfdb3a7b74af59c4
dmaisy <dmaisy@gmail.com>
2020-02-03 12:19:58 -0300

    v00.03

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts ('js/sw-Utils.js');
 
 
-const STATIC_CACHE      ='static-v2';
+const STATIC_CACHE      ='static-v1';
 const DYNAMIC_CACHE     ='dynamic-v1';
 const INMUTABLE_CACHE   ='inmutable-v1';
 
